7e2cff36dd60f98065776aa6ab39c6004d463aba
Masaru Koizumi <masaru@mk-mode.com>
2016-07-20 12:03:45 +0900

    UPD: Fixed a comment.

--- lib/mk_sunmoon/compute.rb
+++ lib/mk_sunmoon/compute.rb
@@ -192,7 +192,7 @@ module MkSunmoon
         when ym < sprintf("%04d-%02d", 2012, 7); dt = Const::TT_TAI + 34
         when ym < sprintf("%04d-%02d", 2015, 7); dt = Const::TT_TAI + 35
         when ym < sprintf("%04d-%02d", 2017, 1); dt = Const::TT_TAI + 36
-        when ym < sprintf("%04d-%02d", 2019, 1); dt = Const::TT_TAI + 37  # <= 第27回うるう秒実施までの暫定措置
+        when ym < sprintf("%04d-%02d", 2019, 1); dt = Const::TT_TAI + 37  # <= 第28回うるう秒実施までの暫定措置
         else
           t = y - 2000
           dt  = 62.92    + \
